379fb069827342bbee0471e82154bdf4b0595271
Nicolás Boolls <102563121+Nico2176@users.noreply.github.com>
2022-09-20 14:24:10 -0500

    Add files via upload
    Sistema de ecuaciones de fuente no nula resuelto. Utilicé librería JAMA.

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,12 +6,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.Scanner;
-
+import Jama.Matrix;
+//fuente de memoria no nula!!!
+//el informe que este bien prolijo como se pide en el pdf. vale mucho de la nota. justificar xD
+//si no es ergodica, el sistema de ecuaciones no tendra solucion o similar
 public class Main {
 	public static void main(String[] args) {
-		int[] arreglo= {0,0,0,0,0,0,0,0,0};   //el criterio que usé aca fue que los primeros tres elementos del array van a simbolizar la cantidad de veces que fueron a determinada letra si la anterior era la A.
-		//es decir, para el elemento 0, la cantidad de veces que hay una A despues de una A, para el elemento 1, la cantidad de veces que hay una B despues de una A, y así sucesivamente con los demás simbolos de la fuente.
-		int[] cantSimbolos= {0,0,0};  // lo hice de auxiliar para verificar que estén bien las cantidades
+		double[] arreglo= {0,0,0,0,0,0,0,0,0};   //el criterio que usï¿½ aca fue que los primeros tres elementos del array van a simbolizar la cantidad de veces que fueron a determinada letra si la anterior era la A.
+		//es decir, para el elemento 0, la cantidad de veces que hay una A despues de una A, para el elemento 1, la cantidad de veces que hay una B despues de una A, y asï¿½ sucesivamente con los demï¿½s simbolos de la fuente.
+		int[] cantSimbolos= {0,0,0};  // lo hice de auxiliar para verificar que estï¿½n bien las cantidades
+		double[][] matriz = new double[3][3];
+		double[][] matrizSistema = new double[3][3];
 		try {
 			FileReader arch = new FileReader("archivo.txt");
 			if (arch.ready()) {
@@ -71,6 +76,7 @@ public class Main {
 					
 					System.out.println("P("+ a+ "->"+ b  +")"+"=" + (float) arreglo[i]/cantidad);
 					
+					
 				}
 			/*	System.out.println("------------------");                              //usado para verificar 
 				for (int i=0;i<3;i++) {
@@ -78,7 +84,53 @@ public class Main {
 				}
 				System.out.println("------------------");
 				System.out.println("Cantidad de simbolos totales:" + cantidad); */ 
+				matriz[0][0]=arreglo[0]/(arreglo[0]+arreglo[1]+arreglo[2]);
+				matriz[1][0]=arreglo[1]/(arreglo[0]+arreglo[1]+arreglo[2]);
+				matriz[2][0]=arreglo[2]/(arreglo[0]+arreglo[1]+arreglo[2]);
+				matriz[0][1]=arreglo[3]/(arreglo[3]+arreglo[4]+arreglo[5]);
+				matriz[1][1]=arreglo[4]/(arreglo[3]+arreglo[4]+arreglo[5]);
+				matriz[2][1]=arreglo[5]/(arreglo[3]+arreglo[4]+arreglo[5]);
+				matriz[0][2]=arreglo[6]/(arreglo[6]+arreglo[7]+arreglo[8]);
+				matriz[1][2]=arreglo[7]/(arreglo[6]+arreglo[7]+arreglo[8]);
+				matriz[2][2]=arreglo[8]/(arreglo[6]+arreglo[7]+arreglo[8]);
 				
+				
+				
+				
+				
+				System.out.println("Matriz...");
+				for (int i=0;i<3;i++) { //no nula!!!
+					for (int j=0;j<3;j++) {
+						System.out.print("["+matriz[i][j]+"]"); //"Matriz["+ i+"]["+j+"] ="
+					}
+					System.out.println("");
+				}
+				
+				matrizSistema=matriz;
+				
+				
+				matrizSistema[0][0]--;
+				matrizSistema[1][1]--;
+				matrizSistema[2][2]--;
+				
+				double[] resultadosSistema= {0,0,0};
+			
+				Matrix matrixSystem = new Matrix(matrizSistema);  //libreria jama para resolver sistema de ecuaciones. ver documentacion
+				Matrix resultados = new Matrix(resultadosSistema,3); // documentacion: https://math.nist.gov/javanumerics/jama/doc/
+				
+				Matrix solucion = matrixSystem.solve(resultados);
+				
+				 System.out.println("Matriz Sistema...");  //muestro matriz - matriz identidad
+				for (int i=0;i<3;i++) { //no nula!!!
+					for (int j=0;j<3;j++) {
+						System.out.print("["+matrizSistema[i][j]+"]"); //"Matriz["+ i+"]["+j+"] ="
+					}
+					System.out.println("");
+				} 
+							
+				System.out.println("x = " + solucion.get(0, 0));
+		        System.out.println("y = " + solucion.get(1, 0));
+		        System.out.println("z = " + solucion.get(2, 0));
 			}
 		} catch (Exception e) {
 			System.out.println("Error");
